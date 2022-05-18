4c41a1ac759a3afff0b7a08bcced62151ee35c23
Haashim Ameer <34925045+HaashimAmeer@users.noreply.github.com>
2022-05-17 23:20:35 -0700

    Initial structuring

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const port = 3200;
+
+//routes
+app.get("/home", (req, res) => {
+  res.end("Task Manager App");
+});
+
+// api.get("/api/v1/tasks)          get all the tasks
+// api.post("/api/v1/tasks)         create a new task
+// api.get("/api/v1/tasks/:id)      get single task
+// api.put("/api/v1/tasks/:id)      update single task
+// api.delete("/api/v1/tasks/:id)   delete task
+
+app.listen(port, () => {
+  console.log(`Server is listening to port ${port}...`);
+});
