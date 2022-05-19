3a5b76151ff0cf4769c44950fc98b44e85acd671
Haashim Ameer <34925045+HaashimAmeer@users.noreply.github.com>
2022-05-18 18:00:39 -0700

    Added input validation for POST reqs

--- models/Task.js
+++ models/Task.js
@@ -2,8 +2,16 @@ const mongoose = require("mongoose");
 
 //Structure for schema - data type requirements for <k,v> pair
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "Must provide a name"],
+    trim: true,
+    maxlength: [20, "Name cannot exceed 20 characters"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model("Task", TaskSchema);
